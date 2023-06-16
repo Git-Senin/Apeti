ad5f997b7c239e123ce6e92c4c7df43017b10ba5
Git-Senin <lawrencecole8@yahoo.com>
2023-06-16 16:35:04 -0700

    update mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,28 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <windows.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
+
+
+#ifdef Q_OS_WIN
+    HWND hWnd = reinterpret_cast<HWND>(winId());
+    DWORD dwExStyle = GetWindowLongPtr(hWnd, GWL_EXSTYLE);
+    dwExStyle |= WS_EX_TRANSPARENT | WS_EX_LAYERED;
+    SetWindowLongPtr(hWnd, GWL_EXSTYLE, dwExStyle);
+
+    BYTE currentAlpha;
+    DWORD currentFlags;
+    GetLayeredWindowAttributes(hWnd, nullptr, &currentAlpha, &currentFlags);
+
+    SetLayeredWindowAttributes(hWnd, 0, 255, currentAlpha);
+#endif
 }
 
 MainWindow::~MainWindow()
